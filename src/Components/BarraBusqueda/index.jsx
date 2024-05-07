adb378ebf8598dcdf8cd701aca1bb5e9e2a9a838
VianGalop <matcaptu10@gmail.com>
2024-05-07 14:50:07 -0600

    diseño modal busqueda

--- src/Components/BarraBusqueda/index.jsx
+++ src/Components/BarraBusqueda/index.jsx
@@ -1,26 +1,8 @@
-import React, { useEffect, useState } from "react"
+import React from 'react'
 
-export default function App() {
-  const  [openMenu, setOpenMenu] = useState(true)
-
-  const onClose = (e)=>{
-   e.stopPropagation()
-   setOpenMenu(false)
-  }
-   
-useEffect(()=>{
-    console.log(openMenu);
-},[openMenu,setOpenMenu])
-  
+export const Sidebar = ({onClose}) => {
   return (
-     <div className="w-full flex flex-col px-[11px] py-[18px] bg-[]">
-        <div className="flex flex-row justify-between">
-          <button className= "bg-[#6E707A] w-[161px] h-[40px]  hover:bg-slate-400  text-base px-[18px] y-[11px] text-zinc-200 " onClick={setOpenMenu}>Search for places</button>
-          <button className="flex items-center justify-center bg-[#6E707A] hover:bg-slate-400 font-medium h-[40px] w-[40px] text-base rounded-full box__shadow">
-          <svg stroke="currentColor" fill="currentColor" strokeWidth="0" viewBox="0 0 24 24" className="h-[22px] w-[22px]" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path fill="none" d="M0 0h24v24H0V0z"></path><path d="M12 8c-2.21 0-4 1.79-4 4s1.79 4 4 4 4-1.79 4-4-1.79-4-4-4zm8.94 3A8.994 8.994 0 0013 3.06V1h-2v2.06A8.994 8.994 0 003.06 11H1v2h2.06A8.994 8.994 0 0011 20.94V23h2v-2.06A8.994 8.994 0 0020.94 13H23v-2h-2.06zM12 19c-3.87 0-7-3.13-7-7s3.13-7 7-7 7 3.13 7 7-3.13 7-7 7z"></path></svg>
-          </button>
-        </div>
-         <aside id='modal' className={`absolute w-full min-h-screen h-full  flex-shrink-0 ${!openMenu && 'hidden'}`}>
+    <aside id='modal' className={`absolute w-full min-h-screen h-full  flex-shrink-0 ${!openMenu && 'hidden'}`}>
             <div className="border-r-2  w-full h-full bg-[#1E213A] dark:border-none">
               <div className="flex flex-row justify-end py-3 px-3">
                  <button className="flex items-center justify-center text-silver text-white hover:bg-slate-400 font-medium h-[40px] w-[40px] text-base rounded-full box__shadow" onClick={onClose}>
@@ -51,22 +33,5 @@ useEffect(()=>{
               </ul>
             </div>
         </aside>
-        <div className="self-center  mt-[46px]  mb-[10px] 2xl:mt-[76px] opacity-5 translate-y-14" >
-            <img src='./images/Shower.png' alt="weather" width="150" height="174" />
-        </div>
-        <div className="text-center">
-          <h1 className="text-silver font-medium text-[120px]">15<span className="text-[] text-5xl duration-700">C</span></h1>
-          <h5 className="text-2xl text-[#A09FB1] font-semibold mt-[13px] capitalize">Shower</h5>    
-         <div className="flex flex-row gap-[16px] justify-center text-[#88869D] text-lg mt-[48px] mb-[33px]">
-            <p>Today</p>
-            <p>-</p>
-            <p>May 24</p>
-          </div>
-          <div className="flex flex-row gap-2 justify-center text-[#88869D] ">
-            <p>location</p>
-          </div>
-        </div>
-    </div>    
   )
 }
-
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,26 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        'bg-weather-days':'#100E1D',
+        'bg-weather':'#1E213A',
+        'bg-boton-search':'#6E707A',
+        'bg-rectangulos':'#1E213A',
+        'color-place':'#A09FB1',
+        'color-date':'#88869D',
+        'color-text':'#E7E7EB',
+        'color-text-small':'#A09FB1'
+      },
+      fontFamily:{
+        'Raleway':['Raleway'],
+        'Fraunces':['Fraunces','serif'],
+      },
+    },
   },
   plugins: [],
 }
