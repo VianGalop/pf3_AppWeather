bf6b8c712812b2e6eb6bc005944951626a8b8f4d
VianGalop <matcaptu10@gmail.com>
2024-05-10 11:19:26 -0600

    sol

--- src/Components/DaysWeather/index.jsx
+++ src/Components/DaysWeather/index.jsx
@@ -5,11 +5,11 @@ export const DaysWather = () => {
 
   return (
     <div className="px-10 py-9 grid grid-cols-2 gap-[22px] lg:grid-cols-4 xl:grid-cols-5 xl:px-0">
-          <Days day={'Tomorrow'} urlImg={''} gradMin={'9'} gradMax={'20'} />
-          <Days day={'Sun 7 Jun'} urlImg={''} gradMin={'16'} gradMax={'11'} />
-          <Days day={'Mon 8 Jun'} urlImg={''} gradMin={'16'} gradMax={'11'} />
-          <Days day={'Tue 9 Jun'} urlImg={''} gradMin={'16'} gradMax={'11'} />
-          <Days day={'Wed 10 Jun'} urlImg={''} gradMin={'16'} gradMax={'11'} />          
+          <Days day={'Tomorrow'} urlImg={'./images/02d.png'} gradMin={'9'} gradMax={'20'} />
+          <Days day={'Sun 7 Jun'} urlImg={'./images/02d.png'} gradMin={'16'} gradMax={'11'} />
+          <Days day={'Mon 8 Jun'} urlImg={'./images/02d.png'} gradMin={'16'} gradMax={'11'} />
+          <Days day={'Tue 9 Jun'} urlImg={'./images/02d.png'} gradMin={'16'} gradMax={'11'} />
+          <Days day={'Wed 10 Jun'} urlImg={'./images/02d.png'} gradMin={'16'} gradMax={'11'} />          
     </div>
   )
 }
